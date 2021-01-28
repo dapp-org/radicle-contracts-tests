f569112be0a7cd8254405c20e6820224121e58ab
David Terry <me@xwvvvvwx.com>
2021-01-28 12:41:09 +0100

    VestingToken: test withdrawn accounting

--- src/RadicleTests.t.sol
+++ src/RadicleTests.t.sol
@@ -67,7 +67,6 @@ contract VestingTokenTests is DSTest, DSMath {
 
     // `withdrawableBalance()` should always return the actual amount that will
     // be withdrawan when calling `withdrawVested()`
-    // TODO: allow `hevm.warp` for symbolic values
     function test_withdrawal_amount(
         uint24 jump, uint24 amount, uint8 startOffset, uint24 vestingPeriod, uint24 cliffPeriod
     ) public {
@@ -137,10 +136,49 @@ contract VestingTokenTests is DSTest, DSMath {
             "beneficiary has received all vested tokens"
         );
         assertEq(
-            rad.balanceOf(address(owner)),
-            unvested,
+            rad.balanceOf(address(owner)), unvested,
             "owner has received all unvested tokens"
         );
+    }
+
+    // The `withdrawn` attribute should always accurately reflect the actual amount withdrawn
+    // Demonstrates a bug where the withdrawn attribute is set to a misleading value after termination
+    function test_withdrawn_accounting(
+        uint8 jump, uint24 amount, uint8 startOffset, uint24 vestingPeriod, uint24 cliffPeriod
+    ) public {
+        if (vestingPeriod == 0) return;
+        if (startOffset == 0) return;
+        if (amount == 0) return;
+        if (amount > 10000000 ether) return;
+
+        VestingToken vest = Utils.mkVestingToken(
+            address(rad),
+            address(owner),
+            address(user),
+            amount,
+            block.timestamp - startOffset,
+            vestingPeriod,
+            cliffPeriod
+        );
+
+        uint withdrawn = 0;
+
+        for (uint i; i < 10; i++) {
+            hevm.warp(block.timestamp + jump);
+            uint prebal = rad.balanceOf(address(user));
+            user.withdrawVested(vest);
+
+            uint postbal = rad.balanceOf(address(user));
+            withdrawn = add(withdrawn, postbal - prebal);
+        }
+
+        assertEq(withdrawn, vest.withdrawn(), "pre-termination");
+
+        hevm.warp(block.timestamp + jump);
+        uint withdrawable = vest.withdrawableBalance();
+        owner.terminateVesting(vest);
+
+        assertEq(vest.withdrawn(), add(withdrawn, withdrawable), "post-termination");
     }
 }
 
