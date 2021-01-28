0931bbaf5f72b9287aa3e961815cc635127430ca
David Terry <me@xwvvvvwx.com>
2021-01-28 11:56:20 +0100

    moar vestingToken tests

--- src/RadicleTests.t.sol
+++ src/RadicleTests.t.sol
@@ -10,23 +10,30 @@ import {ENS}           from "@ensdomains/ens/contracts/ENS.sol";
 import {ENSRegistry}   from "@ensdomains/ens/contracts/ENSRegistry.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
-import "ds-test/test.sol";
+import {DSTest} from "ds-test/test.sol";
+import {DSMath} from "ds-math/math.sol";
 
 interface Hevm {
     function warp(uint256) external;
     function store(address,bytes32,bytes32) external;
 }
 
-contract VestingUser is DSTest {
+contract VestingUser {
     function withdrawVested(VestingToken vest) public {
         vest.withdrawVested();
     }
 }
 
-contract VestingTokenTests is DSTest {
+contract VestingOwner {
+    function terminateVesting(VestingToken vest) public {
+        vest.terminateVesting();
+    }
+}
+
+contract VestingTokenTests is DSTest, DSMath {
     RadicleToken rad;
-    VestingToken vest;
     VestingUser user;
+    VestingOwner owner;
     Hevm hevm = Hevm(HEVM_ADDRESS);
 
     function setUp() public {
@@ -34,37 +41,22 @@ contract VestingTokenTests is DSTest {
 
         rad = new RadicleToken(address(this));
         user = new VestingUser();
-
-        // VestingToken calls transferFrom in the constructor, so we use
-        // create2 to precompute the address of the token and approve the
-        // VestingToken to move rad on behalf of the testing contract
-        bytes32 salt = bytes32("0xacab");
-        address token = address(rad);
-        address owner = address(this);
-        address beneficiary = address(user);
-        uint amount = 1000000 ether;
-        uint vestingStartTime = block.timestamp - 1;
-        uint vestingPeriod = 2 weeks;
-        uint cliffPeriod = 1 days;
-
-        address vestAddress = Utils.create2Address(
-            salt,
-            address(this),
-            type(VestingToken).creationCode,
-            abi.encode(
-                token, owner, beneficiary, amount, vestingStartTime, vestingPeriod, cliffPeriod
-            )
-        );
-
-        rad.approve(vestAddress, uint(-1));
-        vest = new VestingToken{salt: salt}(
-            token, owner, beneficiary, amount, vestingStartTime, vestingPeriod, cliffPeriod
-        );
+        owner = new VestingOwner();
     }
 
     // Demonstrates a bug where withdrawableBalance() always reverts after
     // vesting has been interrupted.
     function testFail_vesting_failure() public {
+        VestingToken vest = Utils.mkVestingToken(
+            address(rad),
+            address(this),
+            address(user),
+            10000000 ether,
+            block.timestamp - 1,
+            2 weeks,
+            1 days
+        );
+
         hevm.warp(block.timestamp + 2 days);
         vest.terminateVesting();
         hevm.warp(block.timestamp + 1 days);
@@ -76,7 +68,24 @@ contract VestingTokenTests is DSTest {
     // `withdrawableBalance()` should always return the actual amount that will
     // be withdrawan when calling `withdrawVested()`
     // TODO: allow `hevm.warp` for symbolic values
-    function test_withdrawal_amount(uint24 jump) public {
+    function test_withdrawal_amount(
+        uint24 jump, uint24 amount, uint8 startOffset, uint24 vestingPeriod, uint24 cliffPeriod
+    ) public {
+        if (vestingPeriod == 0) return;
+        if (startOffset == 0) return;
+        if (amount == 0) return;
+        if (amount > 10000000 ether) return;
+
+        VestingToken vest = Utils.mkVestingToken(
+            address(rad),
+            address(this),
+            address(user),
+            amount,
+            block.timestamp - startOffset,
+            vestingPeriod,
+            cliffPeriod
+        );
+
         hevm.warp(block.timestamp + jump);
 
         uint amt = vest.withdrawableBalance();
@@ -85,7 +94,53 @@ contract VestingTokenTests is DSTest {
         user.withdrawVested(vest);
         uint postbal = rad.balanceOf(address(user));
 
-        assertEq(postbal - prebal, amt);
+        assertEq(postbal - prebal, amt, "withdrawn amount matches withdrawableBalance");
+    }
+
+    // The VestingToken should be empty after `terminateVesting()` has been called
+    // The beneficiary should have received all vested tokens
+    // The owner should have received all unvested tokens
+    function test_empty_after_termination(
+        uint24 jump, uint24 amount, uint8 startOffset, uint24 vestingPeriod, uint24 cliffPeriod
+    ) public {
+        if (vestingPeriod == 0) return;
+        if (startOffset == 0) return;
+        if (amount == 0) return;
+        if (amount > 10000000 ether) return;
+
+        VestingToken vest = Utils.mkVestingToken(
+            address(rad),
+            address(owner),
+            address(user),
+            amount,
+            block.timestamp - startOffset,
+            vestingPeriod,
+            cliffPeriod
+        );
+
+        hevm.warp(block.timestamp + jump);
+
+        assertEq(rad.balanceOf(address(vest)), amount);
+        uint vested = vest.withdrawableBalance();
+        log_named_uint("vested", vested);
+        log_named_uint("amount", amount);
+        uint unvested = sub(amount, vest.withdrawableBalance());
+
+        owner.terminateVesting(vest);
+
+        assertEq(
+            rad.balanceOf(address(vest)), 0,
+            "vesting token is empty"
+        );
+        assertEq(
+            rad.balanceOf(address(user)), vested,
+            "beneficiary has received all vested tokens"
+        );
+        assertEq(
+            rad.balanceOf(address(owner)),
+            unvested,
+            "owner has received all unvested tokens"
+        );
     }
 }
 
@@ -133,5 +188,33 @@ library Utils {
             salt,
             keccak256(abi.encodePacked(creationCode, args))
         ))));
+    }
+
+    function mkVestingToken(
+        address token,
+        address owner,
+        address beneficiary,
+        uint amount,
+        uint vestingStartTime,
+        uint vestingPeriod,
+        uint cliffPeriod
+    ) internal returns (VestingToken) {
+        bytes32 salt = bytes32("0xacab");
+
+        address vestAddress = Utils.create2Address(
+            salt,
+            address(this),
+            type(VestingToken).creationCode,
+            abi.encode(
+                token, owner, beneficiary, amount, vestingStartTime, vestingPeriod, cliffPeriod
+            )
+        );
+
+        RadicleToken(token).approve(vestAddress, uint(-1));
+        VestingToken vest = new VestingToken{salt: salt}(
+            token, owner, beneficiary, amount, vestingStartTime, vestingPeriod, cliffPeriod
+        );
+
+        return vest;
     }
 }
